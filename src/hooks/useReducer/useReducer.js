4afed8c51a0fb8f706b45a0a72dff5d5ac767a3c
harshana-lk <harshuonlive@gmail.com>
2024-03-29 15:13:44 +0530

    🔖Implemented useReducer hook

---
+++ src/hooks/useReducer/useReducer.js
@@ -0,0 +1,34 @@
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1, showText: state.showText };
+
+    case "TOGGLE_SHOW_TEXT":
+      return { count: state.count, showText: !state.showText };
+
+    default:
+      return state;
+  }
+};
+
+const useReducer = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h3>Count: {state.count}</h3>
+      <button
+        onClick={() => {
+          dispatch({ type: "INCREMENT" });
+          dispatch({ type: "TOGGLE_SHOW_TEXT" });
+        }}
+      >
+        Click here
+      </button>
+      {state.showText && <p>This is a text</p>}
+    </div>
+  );
+};
+export default useReducer;
